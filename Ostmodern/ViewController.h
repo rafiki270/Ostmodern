b1999d4c07ed6565f6ee7a9936ce3e9af6aeb5b7
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2016-01-22 12:53:43 +0000

    Initial Commit

---
+++ Ostmodern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ostmodern
+//
+//  Created by Ondrej Rafaj on 22/01/2016.
+//  Copyright © 2016 Ridiculous Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OstmodernTests/OstmodernTests.m
@@ -0,0 +1,39 @@
+//
+//  OstmodernTests.m
+//  OstmodernTests
+//
+//  Created by Ondrej Rafaj on 22/01/2016.
+//  Copyright © 2016 Ridiculous Innovations. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OstmodernTests : XCTestCase
+
+@end
+
+@implementation OstmodernTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
